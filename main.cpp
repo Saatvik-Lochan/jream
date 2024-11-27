7fcbb92664161ed028c1760568ed9de8e00e8a0f
Saatvik Lochan <saatviklochan@gmail.com>
2024-11-27 14:23:10 +0000

    hello world loaded in memory

---
+++ main.cpp
@@ -0,0 +1,63 @@
+// This code has heavily borrowed from:
+// https://eli.thegreenplace.net/2013/11/05/how-to-jit-an-introduction
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+// memory allocated on the page boundary
+void *alloc_writable_memory(size_t size) {
+  void *ptr =
+      mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ptr == (void *)-1) {
+    perror("mmap");
+    return NULL;
+  }
+  return (unsigned char*) ptr;
+}
+
+// sets a RX permission on the given memory, which must be page-aligned
+int make_memory_executable(void *m, size_t size) {
+  if (mprotect(m, size, PROT_READ | PROT_EXEC) == -1) {
+    perror("mprotect");
+    return -1;
+  }
+  return 0;
+}
+
+void emit_code_into_memory(unsigned char* m) {
+
+  // riscv-64 code for the write syscall
+  unsigned char code[] = {
+    0x13, 0x86, 0x05, 0x00, // mv a2, a1
+    0x93, 0x05, 0x05, 0x00, // mv a1, a0
+    0x13, 0x05, 0x10, 0x00, // li a0, 1
+    0x93, 0x08, 0x00, 0x04, // li a7, 63
+    0x73, 0x00, 0x00, 0x00, // ecall
+    0x67, 0x80, 0x00, 0x00  // ret
+  };
+
+  memcpy(m, code, sizeof(code));
+
+}
+
+const size_t SIZE = 1024;
+typedef void (*JittedFunc)(const char*, int);
+
+// Allocates RW memory, emits the code into it and sets it to RX before
+// executing.
+void emit_to_rw_run_from_rx() {
+
+  void *m = alloc_writable_memory(SIZE);
+  emit_code_into_memory((unsigned char*) m);
+  make_memory_executable(m, SIZE);
+
+  const char *text = "Hello World!\n";
+  const int len = strlen(text);
+  JittedFunc func = (JittedFunc) m;
+  func(text, len);
+}
+
+int main() {
+  emit_to_rw_run_from_rx();
+}
